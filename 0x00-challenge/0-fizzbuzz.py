85bc35731c767b0608a9b014a3139083a4bb49b2
realFrankieWilson <frankuwill101@gmail.com>
2023-10-28 21:38:32 +0100

    hot fix

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -27,7 +27,6 @@ def fizzbuzz(n):
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
-
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
